9d35a8ed3504f1fb18d0225aad1278eff3873c75
Stefan Bodewig <stefan.bodewig@freenet.de>
2010-09-13 11:37:08 +0000

    don't match test nodes more than once
    git-svn-id: http://svn.code.sf.net/p/xmlunit/code/trunk/xmlunit@470 458802cb-8328-0410-b1d9-a76001e3bb52

--- src/tests/java-legacy/org/custommonkey/xmlunit/test_Diff.java
+++ src/tests/java-legacy/org/custommonkey/xmlunit/test_Diff.java
@@ -941,7 +941,7 @@ public class test_Diff extends TestCase{
     /**
      * @see https://sourceforge.net/tracker/?func=detail&aid=2807167&group_id=23187&atid=377768
      */
-    public void NOtestIssue2807167() throws Exception {
+    public void testIssue2807167() throws Exception {
         String test = "<tag>" +
             "<child amount=\"100\" />" +
             "<child amount=\"100\" />" +
