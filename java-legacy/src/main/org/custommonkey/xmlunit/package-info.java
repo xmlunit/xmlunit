fe27676f4b039ed3bcb44941b91935ea647239f0
Stefan Bodewig <stefan.bodewig@freenet.de>
2015-01-05 07:14:05 +0100

    package.html => package-info.java

---
+++ java-legacy/src/main/org/custommonkey/xmlunit/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Root of the XMLUnit 1.x compatibility layer.
+ *
+ * <p>This package contains an API compatible with XMLUnit 1.x but
+ * using XMLUnit Core 2.x under the covers.</p>
+ */
+package org.custommonkey.xmlunit;
