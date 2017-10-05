a8188ea749d2fa0673bdb1bd6677a69f6d2158a3
Zheng Wang <zheng-wang@users.noreply.github.com>
2017-10-05 21:27:30 +0800

    handle empty element for ignore placeholder

--- xmlunit-exceptional-node-matchers/src/main/java/org/xmlunit/exnm/PlaceholderDifferenceEvaluator.java
+++ xmlunit-exceptional-node-matchers/src/main/java/org/xmlunit/exnm/PlaceholderDifferenceEvaluator.java
@@ -1,5 +1,6 @@
 package org.xmlunit.exnm;
 
+import org.w3c.dom.Node;
 import org.xmlunit.diff.Comparison;
 import org.xmlunit.diff.ComparisonResult;
 import org.xmlunit.diff.ComparisonType;
@@ -10,19 +11,46 @@ import org.xmlunit.diff.DifferenceEvaluator;
  */
 public class PlaceholderDifferenceEvaluator implements DifferenceEvaluator {
     private static final String PLACEHOLDER_REGEX_IGNORE = "\\$\\{[\\s]*xmlunit\\.ignore[\\s]*}";
+
     public ComparisonResult evaluate(Comparison comparison, ComparisonResult outcome) {
+        Comparison.Detail controlDetails = comparison.getControlDetails();
+        Node controlTarget = controlDetails.getTarget();
+        Comparison.Detail testDetails = comparison.getTestDetails();
+
         if (comparison.getType() == ComparisonType.TEXT_VALUE) {
-            String controlTextValue = (String) comparison.getControlDetails().getValue();
-            String controlTextValueWithIgnorePlaceholderRemoved = controlTextValue.replaceFirst(PLACEHOLDER_REGEX_IGNORE, "");
-            if (controlTextValueWithIgnorePlaceholderRemoved.length() == 0) {    //  the ignore placeholder is all that in the control text node
-                return ComparisonResult.EQUAL;
-            } else if (controlTextValueWithIgnorePlaceholderRemoved.length() != controlTextValue.length()) {    //  there is other content than the ignore placeholder in the control text node
-                throw new RuntimeException("${xmlunit.ignore} must exclusively occupy the text node.");
-            } else {                         //  there is no ignore placeholder in the control text node
-                return outcome;
-            }
+            String controlTextValue = (String) controlDetails.getValue();
+            return evaluateConsideringIgnorePlaceholder(controlTextValue, outcome);
+        } else if (comparison.getType() == ComparisonType.CHILD_NODELIST_LENGTH &&
+                Integer.valueOf(1).equals(controlDetails.getValue()) &&
+                Integer.valueOf(0).equals(testDetails.getValue()) &&
+                controlTarget.getFirstChild().getNodeType() == Node.TEXT_NODE) {
+            String controlNodeChildValue = controlTarget.getFirstChild().getNodeValue();
+            return evaluateConsideringIgnorePlaceholder(controlNodeChildValue, outcome);
+        } else if (comparison.getType() == ComparisonType.CHILD_LOOKUP && controlTarget != null &&
+                controlTarget.getNodeType() == Node.TEXT_NODE) {
+            String controlNodeValue = controlTarget.getNodeValue();
+            return evaluateConsideringIgnorePlaceholder(controlNodeValue, outcome);
         } else {
             return outcome;
+        }
+    }
+
+    private ComparisonResult evaluateConsideringIgnorePlaceholder(String controlText, ComparisonResult outcome) {
+        if (isIgnorePlaceholder(controlText)) {
+            return ComparisonResult.EQUAL;
+        } else {
+            return outcome;
+        }
+    }
+
+    private boolean isIgnorePlaceholder(String text) {
+        String textWithIgnorePlaceholderRemoved = text.replaceFirst(PLACEHOLDER_REGEX_IGNORE, "");
+        if (textWithIgnorePlaceholderRemoved.length() == 0) {    //  the ignore placeholder is all that in the text
+            return true;
+        } else if (textWithIgnorePlaceholderRemoved.length() != text.length()) {    //  there is other content than the ignore placeholder in the text
+            throw new RuntimeException("${xmlunit.ignore} must exclusively occupy the text node.");
+        } else {                         //  there is no ignore placeholder in the text
+            return false;
         }
     }
 }
--- xmlunit-exceptional-node-matchers/src/test/java/org/xmlunit/exnm/PlaceholderDifferenceEvaluatorTest.java
+++ xmlunit-exceptional-node-matchers/src/test/java/org/xmlunit/exnm/PlaceholderDifferenceEvaluatorTest.java
@@ -3,10 +3,9 @@ package org.xmlunit.exnm;
 import org.junit.Test;
 import org.xmlunit.XMLUnitException;
 import org.xmlunit.builder.DiffBuilder;
-import org.xmlunit.diff.ComparisonResult;
-import org.xmlunit.diff.Diff;
-import org.xmlunit.diff.Difference;
+import org.xmlunit.diff.*;
 
+import javax.xml.namespace.QName;
 import java.util.Iterator;
 
 import static junit.framework.TestCase.assertTrue;
@@ -46,6 +45,40 @@ public class PlaceholderDifferenceEvaluatorTest {
     }
 
     @Test
+    public void regression_NoPlaceholder_Different_EmptyExpectedElement() throws Exception {
+        String control = "<elem1><elem11/></elem1>";
+        String test = "<elem1><elem11>abc</elem11></elem1>";
+        Diff diff = DiffBuilder.compare(control).withTest(test)
+                .withDifferenceEvaluator(new PlaceholderDifferenceEvaluator()).build();
+
+        assertTrue(diff.hasDifferences());
+        int count = 0;
+        Iterator it = diff.getDifferences().iterator();
+        while (it.hasNext()) {
+            count++;
+            Difference difference = (Difference) it.next();
+            assertEquals(ComparisonResult.DIFFERENT, difference.getResult());
+
+            Comparison comparison = difference.getComparison();
+            if (count == 1) {
+                String xpath = "/elem1[1]/elem11[1]";
+                assertEquals(ComparisonType.CHILD_NODELIST_LENGTH, comparison.getType());
+                assertEquals(xpath, comparison.getControlDetails().getXPath());
+                assertEquals(0, comparison.getControlDetails().getValue());
+                assertEquals(xpath, comparison.getTestDetails().getXPath());
+                assertEquals(1, comparison.getTestDetails().getValue());
+            } else {
+                assertEquals(ComparisonType.CHILD_LOOKUP, comparison.getType());
+                assertEquals(null, comparison.getControlDetails().getXPath());
+                assertEquals(null, comparison.getControlDetails().getValue());
+                assertEquals("/elem1[1]/elem11[1]/text()[1]", comparison.getTestDetails().getXPath());
+                assertEquals(QName.valueOf("#text"), comparison.getTestDetails().getValue());
+            }
+        }
+        assertEquals(2, count);
+    }
+
+    @Test
     public void hasIgnorePlaceholder_Equal_NoWhitespaceInPlaceholder() throws Exception {
         String control = "<elem1><elem11>${xmlunit.ignore}</elem11></elem1>";
         String test = "<elem1><elem11>abc</elem11></elem1>";
@@ -61,6 +94,22 @@ public class PlaceholderDifferenceEvaluatorTest {
         String test = "<elem1><elem11>abc</elem11></elem1>";
         Diff diff = DiffBuilder.compare(control).withTest(test)
                 .withDifferenceEvaluator(new PlaceholderDifferenceEvaluator()).build();
+
+        assertFalse(diff.hasDifferences());
+    }
+
+    @Test
+    public void hasIgnorePlaceholder_Equal_EmptyActualElement() throws Exception {
+        String control = "<elem1><elem11>${xmlunit.ignore}</elem11></elem1>";
+        String test = "<elem1><elem11/></elem1>";
+        Diff diff = DiffBuilder.compare(control).withTest(test)
+                .withDifferenceEvaluator(new PlaceholderDifferenceEvaluator()).build();
+
+        Iterator it = diff.getDifferences().iterator();
+        while (it.hasNext()) {
+            Difference difference = (Difference) it.next();
+            System.out.println(difference);
+        }
 
         assertFalse(diff.hasDifferences());
     }
