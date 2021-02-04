78230a59e10d52b821d9155e95c6ae13a6f26a4d
Stefan Bodewig <stefan.bodewig@innoq.com>
2021-02-04 14:21:27 +0100

    AssertJ compat tests for xmlunit-assertj3 module

--- compat-tests/assertj-test.sh
+++ compat-tests/assertj-test.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 -o "$#" -gt 3 ]; then
     echo "Usage: assertj-test.sh XMLUNIT_VERSION ASSERTJ_VERSION"
     exit 1
 fi
 
 XMLUNIT_VERSION=$1
 ASSERTJ_VERSION=$2
+if [ "$#" -eq 3 ]; then
+    RUN_ASSERTJ3_MODULE=t
+fi
+
 
 SCRATCH_DIR=scratch/assertj-${ASSERTJ_VERSION}
 
@@ -80,3 +84,72 @@ cp TestResources.java ${SCRATCH_DIR}/src/test/java/org/xmlunit/
 cp -r ../xmlunit-assertj/src/test/java/org/xmlunit/assertj ${SCRATCH_DIR}/src/test/java/org/xmlunit
 
 mvn -f ${SCRATCH_DIR}/pom.xml test
+
+if [ -n "${RUN_ASSERTJ3_MODULE}" ]; then
+    SCRATCH_DIR=scratch/assertj3-${ASSERTJ_VERSION}
+
+    rm -rf scratch && mkdir -p ${SCRATCH_DIR}/src/test/java/org/xmlunit/
+
+    cat > ${SCRATCH_DIR}/pom.xml <<EOF
+<?xml version="1.0"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+
+  <parent>
+    <groupId>org.xmlunit</groupId>
+    <artifactId>xmlunit-parent</artifactId>
+    <version>${XMLUNIT_VERSION}</version>
+    <relativePath>../../..</relativePath>
+  </parent>
+
+  <groupId>org.xmlunit</groupId>
+  <artifactId>xmlunit-compat-tests-assertj3-${ASSERTJ_VERSION}</artifactId>
+  <packaging>jar</packaging>
+  <name>org.xmlunit:xmlunit-compat-tests-assertj3-${ASSERTJ_VERSION}</name>
+  <description>Verifies the AssertJ 3.x assertions are compatible with AssertJ ${ASSERTJ_VERSION}</description>
+  <url>https://www.xmlunit.org/</url>
+
+  <properties>
+    <automatic.module.name>\${project.groupId}.compat-tests-assertj3-${ASSERTJ_VERSION}</automatic.module.name>
+    <assertj.version>${ASSERTJ_VERSION}</assertj.version>
+    <maven.compile.source>8</maven.compile.source>
+    <maven.compile.target>8</maven.compile.target>
+  </properties>
+
+  <dependencies>
+    <dependency>
+      <groupId>org.xmlunit</groupId>
+      <artifactId>xmlunit-core</artifactId>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.xmlunit</groupId>
+      <artifactId>xmlunit-assertj3</artifactId>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.assertj</groupId>
+      <artifactId>assertj-core</artifactId>
+      <version>\${assertj.version}</version>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.mockito</groupId>
+      <artifactId>mockito-core</artifactId>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+</project>
+EOF
+
+    cp TestResources.java ${SCRATCH_DIR}/src/test/java/org/xmlunit/
+
+    cp -r ../xmlunit-assertj3/src/test/java/org/xmlunit/assertj3 ${SCRATCH_DIR}/src/test/java/org/xmlunit
+
+    mvn -f ${SCRATCH_DIR}/pom.xml test
+fi
